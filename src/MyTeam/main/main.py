7ba3a9cecf0334051e3f73caf4059f18a76648be
xiransong <xiransong@outlook.com>
2024-12-14 16:55:55 -0600

    change package name to myteam

---
+++ src/MyTeam/main/main.py
@@ -0,0 +1,18 @@
+def main():
+
+    print("hello world!")
+
+    # parse config
+
+    # init agents and team
+
+    # init dataset
+
+    # inference
+
+    # evaluation
+
+
+
+if __name__ == "__main__":
+    main()
